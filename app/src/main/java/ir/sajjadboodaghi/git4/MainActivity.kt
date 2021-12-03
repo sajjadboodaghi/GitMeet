0de9cc7bcd37fa7f75c5966a407622d4ee6b42af
sajjad boodaghi <sajjad.boodaghi@gmail.com>
2021-12-03 21:59:46 +0330

    add tap here function

--- app/src/main/java/ir/sajjadboodaghi/git4/MainActivity.kt
+++ app/src/main/java/ir/sajjadboodaghi/git4/MainActivity.kt
@@ -2,11 +2,19 @@ package ir.sajjadboodaghi.git4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun showAlert(view: View) {
+        Toast
+            .makeText(this, "you tapped there", Toast.LENGTH_SHORT)
+            .show()
     }
 }
