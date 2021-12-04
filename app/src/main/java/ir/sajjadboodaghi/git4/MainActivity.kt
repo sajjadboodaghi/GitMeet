8a96c680a47768dd2268e030495a9fff7b277e07
sajjad boodaghi <sajjad.boodaghi@gmail.com>
2021-12-04 22:04:51 +0330

    add say and bye buttons and name edit text

--- app/src/main/java/ir/sajjadboodaghi/git4/MainActivity.kt
+++ app/src/main/java/ir/sajjadboodaghi/git4/MainActivity.kt
@@ -3,18 +3,26 @@ package ir.sajjadboodaghi.git4
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    val nameEdit: EditText = findViewById(R.id.name_edit)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    fun showAlert(view: View) {
-        Toast
-            .makeText(this, "you tapped there", Toast.LENGTH_SHORT)
+    fun sayHello(view: View) {
+        Toast.makeText(this, "hello {nameEdit.getText().toString()", Toast.LENGTH_SHORT)
+             .show()
+    }
+
+    fun sayBye(view: View) {
+        val name =
+        Toast.makeText(this, "bye {nameEdit.getText().toString()}", Toast.LENGTH_SHORT)
             .show()
     }
 }
